d18088a8bf11a522040ab9ad533fc244b3acaea5
Zen Li <zenli@outlook.com>
2024-03-08 12:54:41 +0800

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const GoogleAuth = require('@loxjs/google-auth');
+const obfuscateEmail = require('./obfuscate-email');
+const obfuscateWallet = require('./obfuscate-wallet');
+const obfuscateHash = require('./obfuscate-hash');
+module.exports = {
+  GoogleAuth,
+  obfuscateEmail,
+  obfuscateWallet,
+  obfuscateHash
+};
---
+++ obfuscate-hash.js
@@ -0,0 +1,7 @@
+const obfuscateHash = function (hash) {
+  if (!hash) {
+    return '';
+  }
+  return `${hash.slice(0, 6)}...${hash.slice(-4)}`;
+};
+module.exports = obfuscateHash;
